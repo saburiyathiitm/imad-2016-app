95aedee52e1baf5cdefcc5100143d3bbf2d0269e
saburiyathiitm <fathima.nassrin@gmail.com>
2016-09-21 15:27:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,9 +11,7 @@ app.get('/', function (req, res) {
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
-app.get('/article-one', function (req, res) {
-  res.send('Article one requested and will  be served here');
-});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
